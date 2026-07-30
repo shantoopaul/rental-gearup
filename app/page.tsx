405b275d02eae952b19b276eb9d68f9715c6dcbb
shantoopaul <shantoopaul@gmail.com>
2026-07-30 21:37:22 +0600

    init: initial project files

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { InteractiveHoverButton } from "@/components/ui/interactive-hover-button";
+
+const Home = () => {
+	return (
+		<div>
+			<InteractiveHoverButton>LET IT RIDE!!!</InteractiveHoverButton>
+		</div>
+	);
+};
+
+export default Home;
